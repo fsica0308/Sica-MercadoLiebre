d4a7aa50213d8328761e220d01aae4bac810c36e
fsica0308 <fsica0308@gmail.com>
2023-08-17 00:04:43 -0300

    Actualizacion home terminado

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
-});
-
 const PORT = 4000;
 
 app.listen(PORT, () => {
     console.log('Server ON en puerto ' + PORT);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
 });
